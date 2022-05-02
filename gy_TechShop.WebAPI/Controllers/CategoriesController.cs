4d001feb12066c9c04d057cfc745d8160e75502b
Batu YURT <asdasd018@hotmail.com>
2022-05-02 18:12:41 +0300

    API basics tested

---
+++ gy_TechShop.WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using gy_TechShop.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gy_TechShop.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _categoryService.GetAll();
+            if (result.Success) return Ok(result);
+            return BadRequest(result);
+        }
+    }
+}
