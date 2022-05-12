d0b480b89237260687c78afd49dc60be97660360
Batu YURT <asdasd018@hotmail.com>
2022-05-13 02:20:11 +0300

    Login register features

--- gy_TechShop.Web/Controllers/LoginController.cs
+++ gy_TechShop.Web/Controllers/LoginController.cs
@@ -1,12 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using gy_TechShop.Business.Abstract;
+using gy_TechShop.Entities.DTOs;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace gy_TechShop.Web.Controllers
 {
     public class LoginController : Controller
     {
+        IAuthService _authService;
+        IHttpContextAccessor _httpContextAccessor;
+        public LoginController(IAuthService authService, IHttpContextAccessor httpContextAccessor)
+        {
+            _authService = authService;
+            _httpContextAccessor = httpContextAccessor;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(UserLoginDto userLoginDto)
+        {
+            var result = _authService.Login(userLoginDto);
+            if (result.Success)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Email,result.Data.Email),
+                    new Claim(ClaimTypes.NameIdentifier,result.Data.Id.ToString())
+                };
+
+                var userIdentity = new ClaimsIdentity(claims, "LoginClaims");
+
+                ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+                await HttpContext.SignInAsync(principal);
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index", "Login");
         }
     }
 }
---
+++ gy_TechShop.Web/Controllers/RegisterController.cs
@@ -0,0 +1,37 @@
+﻿using gy_TechShop.Business.Abstract;
+using gy_TechShop.Entities.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gy_TechShop.Web.Controllers
+{
+    public class RegisterController : Controller
+    {
+
+        IAuthService _authService;
+
+        public RegisterController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(UserRegisterDto UserRegisterDto)
+        {
+            var result = _authService.Register(UserRegisterDto, UserRegisterDto.Password);
+            if (result.Success)
+            {
+                ViewBag.Message = result.Message;
+            }
+            return View("Index");
+        }
+
+
+
+
+    }
+}
